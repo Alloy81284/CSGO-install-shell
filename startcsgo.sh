88e686d8e11dcb05777f0364d493cfe79ee18b59
Alloy <83437602+Alloy81284@users.noreply.github.com>
2022-10-28 23:10:00 +0800

    Update startcsgo.sh

--- startcsgo.sh
+++ startcsgo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/csgo/csgo
-./srcds_run -tickrate 128 -console -maxplayers_override -ip 0.0.0.0 -autoupdate -steam_dir "/home/csgo" -steamcmd_script "/home/csgo/update.txt"
+cd /root/csgo
+./srcds_run -tickrate 128 -console -maxplayers_override -ip 0.0.0.0 -autoupdate -steam_dir "/root" -steamcmd_script "/root/update.txt"
